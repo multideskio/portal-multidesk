105bcde0178af9f39ba0db4ef37aeca2324e2a73
Paulo Henrique <multidesk.io@gmail.com>
2025-03-17 18:57:58 -0300

    "Adiciona rotas para logout, confirmação e grupo admin"

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,10 +13,17 @@ $routes->get("login", 'Login::index');
 $routes->get("registrar", 'Login::registrar');
 $routes->get("confirmar/(:any)", 'Login::confirmar/$1');
 $routes->get("recuperar", 'Login::recuperar');
+$routes->get("logout", 'Login::logout');
 
 $routes->get("logout", 'Login::logout');
 
 $routes->post("login", 'Admin\V1\Login::login');
 $routes->post("registrar", 'Admin\V1\Login::registrar');
+$routes->post("confirmar/(:any)", 'Admin\V1\Login::confirmar/$1');
+
+
+$routes->group("admin", static function ($routes) {
+   $routes->get("/", 'Admin::index');
+});
 
 
